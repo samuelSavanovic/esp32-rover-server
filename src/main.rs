b8592336d4dd4d93bba75c792cea19674cf94c54
samuelSavanovic <samuel.savanovic@gmail.com>
2025-11-30 13:11:37 +0100

    Add futures-util dependency and implement command sending via WebSocket

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,43 @@
 mod protocol;
 mod ws;
 
+use std::{collections::HashMap, sync::{Arc, RwLock}};
+
 use axum::{
-    Router, routing::get
+    Router, extract::{Query, State, ws::Message}, routing::get
 };
+use tokio::sync::mpsc::UnboundedSender;
+
+use crate::protocol::Command;
+#[derive(Clone)]
+struct AppState{
+    tx: Arc<RwLock<Option<UnboundedSender<Message>>>>
+}
 
 #[tokio::main]
 async fn main() {
-    let app = Router::new().route("/ws", get(ws::upgrade));
+    let state = AppState {  tx: Arc::new(RwLock::new(None)) };
+    let app = Router::new().route("/ws", get(ws::upgrade)).route("/cmd", get(send_cmd)).with_state(state);
     let listener = tokio::net::TcpListener::bind("0.0.0.0:9000").await.unwrap();
     axum::serve(listener, app).await.unwrap();
 }
 
+async fn send_cmd(
+    Query(params): Query<HashMap<String, String>>,
+    State(state): State<AppState>,
+) -> String {
+    let l: i16 = params.get("l").and_then(|v| v.parse().ok()).unwrap_or(0);
+    let r: i16 = params.get("r").and_then(|v| v.parse().ok()).unwrap_or(0);
+
+    let cmd_bytes = Command::new(l, r).to_bytes();
+    let msg = Message::Binary(cmd_bytes.into());
+
+    let guard = state.tx.read().unwrap();
+    if let Some(tx) = guard.as_ref() {
+        if tx.send(msg).is_ok() {
+            return format!("Sent command: L={} R={}", l, r);
+        }
+    }
+
+    "No ESP32 connected".into()
+}
--- src/ws/handler.rs
+++ src/ws/handler.rs
@@ -1,18 +1,36 @@
 use axum::extract::ws::{WebSocketUpgrade, WebSocket, Message};
+use tokio::sync::mpsc::unbounded_channel;
 use crate::protocol::Telemetry;
+use futures_util::{sink::SinkExt, stream::StreamExt};
 
-pub async fn upgrade(ws: WebSocketUpgrade) -> axum::response::Response {
-    ws.on_upgrade(handle_socket)
+pub async fn upgrade(ws: WebSocketUpgrade,
+    state: axum::extract::State<crate::AppState>,
+) -> axum::response::Response {
+    ws.on_upgrade(move |socket| async move {
+        handle_socket(socket, state).await;
+    })
 }
+async fn handle_socket(socket: WebSocket,  state: axum::extract::State<crate::AppState>) {
+    let (mut ws_tx, mut ws_rx) = socket.split();
+    let (tx, mut rx) = unbounded_channel::<Message>();
+    *state.tx.write().unwrap() = Some(tx);
 
-async fn handle_socket(mut socket: WebSocket) {
-    while let Some(Ok(msg)) = socket.recv().await {
+    tokio::spawn(async move {
+        while let Some(msg) = rx.recv().await {
+            if ws_tx.send(msg).await.is_err() {
+                println!("ESP32 websocket closed");
+                break;
+            }
+        }
+    });
+
+    while let Some(Ok(msg)) = ws_rx.next().await {
         match msg {
             Message::Binary(b) => {
                 match Telemetry::from_bytes(&b) {
                     Ok(t) => {
                         let dist:u32=t.distance_mm;
-                        println!("Received telemetry distance_mm: {}", dist)
+                        println!("Received telemetry distance_mm: {}", dist);
                     }
                     Err(e) => println!("parse error: {}", e),
                 }
@@ -25,4 +43,5 @@ async fn handle_socket(mut socket: WebSocket) {
             _ => {}
         }
     }
+    *state.tx.write().unwrap() = None;
 }
